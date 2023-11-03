270a051504c7e914f6cee0d6e45938bd0acc113b
EdwardAngert <2408959-EdwardAngert@users.noreply.gitlab.com>
2023-11-03 13:43:14 -0400

    gh-pages branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EdwardAngert', // Usually your GitHub org/user name.
   projectName: 'edwardangert.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
