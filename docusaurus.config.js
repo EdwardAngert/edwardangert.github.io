b34993bb1bb4007475ef2e10a71c1756eba81a40
EdwardAngert <2408959-EdwardAngert@users.noreply.gitlab.com>
2023-11-03 13:34:29 -0400

    github baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://edwardangert.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/edwardangert.github.io/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
