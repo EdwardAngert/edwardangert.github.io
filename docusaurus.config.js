8f1c0d899b5360389838ca540c19f8857eda1803
EdwardAngert <2408959-EdwardAngert@users.noreply.gitlab.com>
2023-11-03 14:23:14 -0400

    change url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://edwardangert.github.io',
+  url: 'https://edwardangert.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
