c68428078427ba0823986cf317fb6a7c031196a4
EdwardAngert <2408959-EdwardAngert@users.noreply.gitlab.com>
2025-08-03 23:04:31 -0400

    catppuccin theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import catppuccin from '@catppuccin/starlight';
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,7 +22,12 @@ export default defineConfig({
 					href: 'https://github.com/EdwardAngert',
 				},
 			],
-			customCss: ['./src/styles/catppuccin.css'],
+			plugins: [
+				catppuccin({
+					dark: { flavor: "mocha", accent: "sky" },
+					light: { flavor: "latte", accent: "sky" }
+				})
+			],
 			sidebar: [
 				{
 					label: 'About Me',
